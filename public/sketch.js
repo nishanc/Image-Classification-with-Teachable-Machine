24fdf0a58236a99fd467699c2cf27fae49310230
Nishan Wickramarathna <nishanw@99x.lk>
2020-05-23 21:58:04 +0530

    final

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,51 @@
+let video;
+let classifier;
+let modelURL = './model/';
+let label = "waiting...";
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+
 function setup() {
-  createCanvas(200, 200)
+  createCanvas(640, 520);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // STEP 2.1: Start classifying
+  classifyVideo();
+}
+
+// STEP 2.2 classify!
+function classifyVideo() {
+  classifier.classify(video, gotResults);
 }
 
 function draw() {
-  background(51);
-  ellipse(mouseX, mouseY, 60, 60);
+  background(0);
+  
+  // Draw the video
+  image(video, 0, 0);
+
+  // STEP 4: Draw the label
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(label, width / 2, height - 16);
+}
+
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  // Something went wrong!
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Store the label and classify again!
+  label = results[0].label;
+  console.log(results);
+  classifyVideo();
 }
